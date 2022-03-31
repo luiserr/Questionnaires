52fd07184e94ebd4189e79a12084a8ed2301c557
Urlieson León <urlieson@territoriumlife.com>
2022-03-31 12:59:31 -0500

    update front field date

--- src/pages/Reports/list.jsx
+++ src/pages/Reports/list.jsx
@@ -38,7 +38,7 @@ import {getDomain} from '../../utils/tools';
 const headers = {
   id: 'Id',
   presentationId: 'Presentación',
-  userId: 'Creador',
+  ownerName: 'Creador',
   createdAt: 'Fecha de creación',
   finishedAt: 'Fecha de finalización'
 };
